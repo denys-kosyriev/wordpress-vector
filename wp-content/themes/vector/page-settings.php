4bd5bdfc6d58258e9857d2737088461e5903f4c6
denys_kosyriev <utross2241@gmail.com>
2023-06-13 23:14:43 +0300

    feature: create 'settings' page

--- wp-content/themes/vector/page-settings.php
+++ wp-content/themes/vector/page-settings.php
@@ -3,106 +3,33 @@
 <main class='settings'>
   <section>
     <div class='container'>
-      <h2>Налаштування інтернету</h2>
-      <div class='swiper swiper-settings'>
-        <div class='swiper-wrapper'>
-          <div class='swiper-slide'>
-            <div class='setting'>
-              <img src='./assets/images/netis.png' alt=''>
-              Настройка роутера
-              <h3>NETIS</h3>
-              <a href='#' class='btn btn-full'>Детальніше</a>
+      <?php if (have_rows('settings')) :
+        while (have_rows('settings')) :
+          the_row(); ?>
+          <h2><?php echo get_sub_field('title') ?></h2>
+          <div class='swiper swiper-settings'>
+            <div class='swiper-wrapper'>
+              <?php if (have_rows('programs')) :
+                while (have_rows('programs')) :
+                  the_row(); ?>
+                  <div class='swiper-slide'>
+                    <div class='setting'>
+                      <img src='<?php echo get_sub_field('logotype') ?>' alt=''>
+                      <?php echo get_sub_field('settings') ?>
+                      <h3><?php echo get_sub_field('title_program') ?></h3>
+                      <?php $link = get_sub_field('button'); ?>
+                      <a href='<?php echo esc_url($link['url']) ?>' target='_blank' class='btn btn-full'>
+                        <?php echo esc_html($link['title']) ?>
+                      </a>
+                    </div>
+                  </div>
+                <?php endwhile;
+              endif; ?>
             </div>
+            <div class='swiper-pagination'></div>
           </div>
-          <div class='swiper-slide'>
-            <div class='setting'>
-              <img src='./assets/images/totolink.png' alt=''>
-              Настройка роутера
-              <h3>TOPOLINK</h3>
-              <a href='#' class='btn btn-full'>Детальніше</a>
-            </div>
-          </div>
-          <div class='swiper-slide'>
-            <div class='setting'>
-              <img src='./assets/images/tplink.png' alt=''>
-              Настройка роутера
-              <h3>TP-Link</h3>
-              <a href='#' class='btn btn-full'>Детальніше</a>
-            </div>
-          </div>
-        </div>
-        <div class='swiper-pagination'></div>
-      </div>
-      <h2>Налаштування PPPoE-з'єднання</h2>
-      <div class='swiper swiper-settings'>
-        <div class='swiper-wrapper'>
-          <div class='swiper-slide'>
-            <div class='setting'>
-              <img src='./assets/images/w98.png' alt=''>
-              Налаштування PPPoE-з'єднання
-              <h3> Windows 98/98SE</h3>
-              <a href='#' class='btn btn-full'>Детальніше</a>
-            </div>
-          </div>
-          <div class='swiper-slide'>
-            <div class='setting'>
-              <img src='./assets/images/w2000.png' alt=''>
-              Налаштування PPPoE-з'єднання
-              <h3> Windows 2000</h3>
-              <a href='#' class='btn btn-full'>Детальніше</a>
-            </div>
-          </div>
-          <div class='swiper-slide'>
-            <div class='setting'>
-              <img src='./assets/images/wxp.png' alt=''>
-              Налаштування PPPoE-з'єднання
-              <h3> Windows XP</h3>
-              <a href='#' class='btn btn-full'>Детальніше</a>
-            </div>
-          </div>
-          <div class='swiper-slide'>
-            <div class='setting'>
-              <img src='./assets/images/wvista.png' alt=''>
-              Налаштування PPPoE-з'єднання
-              <h3>Windows Vista</h3>
-              <a href='#' class='btn btn-full'>Детальніше</a>
-            </div>
-          </div>
-          <div class='swiper-slide'>
-            <div class='setting'>
-              <img src='./assets/images/w7.png' alt=''>
-              Налаштування PPPoE-з'єднання
-              <h3>Windows 7</h3>
-              <a href='#' class='btn btn-full'>Детальніше</a>
-            </div>
-          </div>
-          <div class='swiper-slide'>
-            <div class='setting'>
-              <img src='./assets/images/w8.png' alt=''>
-              Налаштування PPPoE-з'єднання
-              <h3>Windows 8</h3>
-              <a href='#' class='btn btn-full'>Детальніше</a>
-            </div>
-          </div>
-          <div class='swiper-slide'>
-            <div class='setting'>
-              <img src='./assets/images/mac.png' alt=''>
-              Налаштування PPPoE-з'єднання
-              <h3> Mac OS X Leopard</h3>
-              <a href='#' class='btn btn-full'>Детальніше</a>
-            </div>
-          </div>
-          <div class='swiper-slide'>
-            <div class='setting'>
-              <img src='./assets/images/ubuntu.png' alt=''>
-              Налаштування PPPoE-з'єднання
-              <h3> Ubuntu</h3>
-              <a href='#' class='btn btn-full'>Детальніше</a>
-            </div>
-          </div>
-        </div>
-        <div class='swiper-pagination'></div>
-      </div>
+        <?php endwhile;
+      endif; ?>
     </div>
   </section>
 </main>
