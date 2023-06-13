c43359a56fdbf856a6b48ce0dca26950e5f77f03
denys_kosyriev <utross2241@gmail.com>
2023-06-13 21:36:06 +0300

    feature: create 'payment-description' page

--- wp-content/themes/vector/page-payment-description.php
+++ wp-content/themes/vector/page-payment-description.php
@@ -4,38 +4,22 @@
   <section>
     <div class='container'>
       <h2>
-        Як сплатити за послуги кабельного телебачення та інтернет через сайт
-        <span>Vektor-plus.com</span>
+        <?php echo get_field('payment_title') ?>
+        <span><?php echo get_field('payment_title_green') ?></span>
       </h2>
       <ul>
-        <li>1. Заходимо на сайт
-          <a href='https://vektor-plus.com' target='_blank'>https://vektor-plus.com</a>
-        </li>
-        <li>
-          2. В меню сайту знаходимо кнопку "Оплати" та натискаємо
-          <img src='./assets/images/payment-description-home.jpg' alt=''>
-        </li>
-        <li>
-          3. У вікні, що зʼявилося, обираємо потрібну нам послугу: "Інтернет послуги", "Послуги кабельного ТБ" або
-          "Загальний пакет послуг"
-          <img src='./assets/images/payment-description-payments1.jpg' alt=''>
-        </li>
-        <li>
-          4. Вводимо необхідні для оплати реквізити: Прізвище, імʼя та по-батькові платника (особи, на чиє імʼя
-          оформлений
-          договір надання послуг); адресу (всі дані обираються із випадаючого списку); особовий рахунок (9 символів,
-          вказано
-          у квитанції або можна уточнити, зателефонувавши за телефоном 050-34-34-593); суму оплати і реквізити
-          банківської
-          картки (номер, термін дії та CVV-код)
-          <img src='./assets/images/payment-description-add-address.jpg' alt=''>
-          <img src='./assets/images/payment-description-payments2.jpg' alt=''>
-          <img src='./assets/images/payment-description-payment.jpg' alt=''>
-        </li>
-        <li>5. Натискаємо "Додати у кошик"</li>
-        <li>6. Натискаємо "Підтвердити"</li>
-        <li>7. Банк визначає спосіб підтвердження (SMS-повідомлення, QR-код, дзвінок на телефон тощо)</li>
-        <li>8. Підтверджуєте платіж</li>
+        <?php if (have_rows('payment_instruction')) :
+          while (have_rows('payment_instruction')) :
+            the_row(); ?>
+            <li><?php echo get_sub_field('text') ?>
+              <?php $link = get_sub_field('link'); ?>
+              <a href='<?php echo esc_url($link['url']) ?>' target='_blank'>
+                <?php echo esc_html($link['title']) ?>
+              </a>
+              <img src='<?php echo get_sub_field('image') ?>' alt=''>
+            </li>
+          <?php endwhile;
+        endif; ?>
       </ul>
     </div>
   </section>
