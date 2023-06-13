b03ff6ccd0ecfb44956da761473adc9784e379dc
denys_kosyriev <utross2241@gmail.com>
2023-06-14 00:45:08 +0300

    feature: create 'settings-iptv' page

--- wp-content/themes/vector/page-settings-iptv.php
+++ wp-content/themes/vector/page-settings-iptv.php
@@ -3,41 +3,33 @@
 <main class='settings-iptv'>
   <section>
     <div class='container'>
-      <h2>Налаштування IPTV телебачення</h2>
-      <p>
-        Шановні абоненти ТРК «Вектор», для Вас надається послуга IPTV, можна переглядати улюблені канали через інтернет
-        на
-        смартфоні та PC/TV використовуючи IPTV-player.
-      </p>
-      <p>
-        Завантажити плеєр можна за посиланням
-        <a href='https://vektor-plus.com/images/playlist_vektor/IpTvPlayer-Custom-setup.exe'>
-          https://vektor-plus.com/images/playlist_vektor/IpTvPlayer-Custom-setup.exe
-        </a>
-      </p>
-      <p>
-        Ця версія iptv плеєра налаштована для використання в нашій мережі, потрібно лише встановити та користуватися.
-        Плейлист із каналами вбудований за замовчуванням.
-      </p>
-      <p>
-        Якщо ви завантажили та встановили IPTV плеєр безпосередньо з сайту розробника, то в даному випадку необхідно
-        буде
-        прописати адресу плейлиста вручну. Запускаємо плеєр, заходимо в налаштування. Вибираємо пункт «Загальні», і в
-        рядку «Адреса каналів» (файл M3U) вставляємо адресу списку каналів
-        <a href='https://iptv.vektor-plus.com/playlist.m3u8'
-           target='_blank'>
-          https://iptv.vektor-plus.com/playlist.m3u8</a>
-        , а також можна додати адресу телепрограми
-        <a href='https://iptv.vektor-plus.com/epg/epg.xml' target='_blank'>
-          https://iptv.vektor-plus.com/epg/epg.xml
-        </a>
-      </p>
-      <img src='./assets/images/iptv.jpg' alt=''>
-      <p>
-        <span>Натискаємо оновити та користуємося.</span>
-        Для мобільних пристроїв ви можете вибрати будь-який IPTV плеєр доступний вам в APP store або Pley маркет,
-        використовуючи вище описані налаштування.
-      </p>
+      <h2><?php echo get_field('settings_iptv_title') ?></h2>
+      <?php if (have_rows('settings_iptv_text_before_image')) :
+        while (have_rows('settings_iptv_text_before_image')) :
+          the_row(); ?>
+          <p>
+            <?php echo get_sub_field('text') ?>
+            <?php $link = get_sub_field('link'); ?>
+            <a href='<?php echo esc_url($link['url']) ?>' target='_blank'>
+              <?php echo esc_html($link['title']) ?>
+            </a>
+            <?php echo get_sub_field('text2') ?>
+            <?php $link2 = get_sub_field('link2'); ?>
+            <a href='<?php echo esc_url($link2['url']) ?>' target='_blank'>
+              <?php echo esc_html($link2['title']) ?>
+            </a>
+          </p>
+        <?php endwhile;
+      endif; ?>
+      <img src='<?php echo get_field('settings_iptv_image') ?>' alt=''>
+      <?php if (have_rows('settings_iptv_text_after_image')) :
+        while (have_rows('settings_iptv_text_after_image')) :
+          the_row(); ?>
+          <p>
+            <?php echo get_sub_field('text') ?>
+          </p>
+        <?php endwhile;
+      endif; ?>
     </div>
   </section>
 </main>
