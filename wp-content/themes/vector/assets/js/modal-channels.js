4b88ca54069d54068353900ce5605ae0fac5cf65
denys_kosyriev <utross2241@gmail.com>
2023-07-08 22:31:34 +0300

    fix: correcting modal window channels

--- wp-content/themes/vector/assets/js/modal-channels.js
+++ wp-content/themes/vector/assets/js/modal-channels.js
@@ -3,9 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
   // Work channels list changes
 
   const buttons = document.getElementsByClassName('btn-switch-channels');
-  const tableChannels = document.getElementsByClassName('scroll');
+  const tableChannels = document.getElementsByClassName('scroll-channels');
   tableChannels[0].classList.remove('d-none')
-
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
         for (let t = 0; t < buttons.length; t++) {
@@ -13,11 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
           tableChannels[t].classList.add('d-none')
         }
         buttons[i].classList.add('active');
-        if (buttons[i].className.includes('analog-btn active')) {
-          tableChannels[i].classList.remove('d-none')
-        } else if (buttons[i].className.includes('digital-btn active')) {
-          tableChannels[i].classList.remove('d-none')
-        }
+        tableChannels[i].classList.remove('d-none');
       }
     )
   }
